6c3e1163e7d75406c2445f74def22b8b4656cdc5
Munnarhh <okenyimax10@gmail.com>
2023-06-24 19:04:35 +0100

    update

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -53,7 +53,7 @@ const String kSuccesss = "assets/gifs/success.gif";
 
 //keys
 // const String kMapKey = "AIzaSyCTsnQMYvepCa1mBh-aYJ6bapBSGN7-NHg";
-const String kMapKey = "AIzaSyAZ7ZkzSDawETIza8EnzK6H5RJjinmlW7g";
+const String kMapKey = "";
 
 const String pp =
     'https://maps.googleapis.com/maps/api/geocode/json?latlng=6.671785162584827,3.1525325969473825&key=AIzaSyAZ7ZkzSDawETIza8EnzK6H5RJjinmlW7g';
